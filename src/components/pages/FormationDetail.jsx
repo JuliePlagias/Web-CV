ff5915084b1f0b17c78fa738371d3b1eba18f75e
JuliePlagias <julieplagias@gmail.com>
2025-02-27 13:07:34 +0100

    Add - Formations json

---
+++ src/components/pages/FormationDetail.jsx
@@ -0,0 +1,18 @@
+export default function FormationsDetails(){
+    return (
+        <div>
+            {/* use JSON */}
+            <h2>Nom formations</h2>
+            <h3>etablissement et lieu</h3>
+            <h3>date</h3>
+            <p>Présentation de la formation</p>
+            <h3>Compétences</h3>
+            <ul>
+                <li>Compétence 1</li>
+                <li>Compétence 2</li>
+                <li>Compétence 3</li>
+                <li>Compétence 4</li>
+            </ul>
+        </div>
+    );
+}
