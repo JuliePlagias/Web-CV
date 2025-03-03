55b7278fdf37e782d0419cf1107d68adedb069da
JuliePlagias <julieplagias@gmail.com>
2025-03-03 13:34:21 +0100

    Upd - style renamed and block style fixed

--- src/components/pages/FormationDetail.jsx
+++ src/components/pages/FormationDetail.jsx
@@ -6,7 +6,7 @@ export default function FormationsDetails(){
     const formationName = Object.keys(data).find((key)=>location.pathname.endsWith(key)); 
 
     return (
-        <div className="details">
+        <div className="formation-details">
             <h2>{ data[formationName].name}</h2>
             <h3>{ data[formationName].place}</h3>
             <h3>{ data[formationName].date}</h3>
