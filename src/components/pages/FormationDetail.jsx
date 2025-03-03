cb52cf9f54ab5ee42476703a84127a0153968733
JuliePlagias <julieplagias@gmail.com>
2025-03-03 14:01:25 +0100

    Upd - Formations details linked to new formation json

--- src/components/pages/FormationDetail.jsx
+++ src/components/pages/FormationDetail.jsx
@@ -2,18 +2,18 @@ import { useLocation } from "react-router-dom";
 
 export default function FormationsDetails(){
     const location = useLocation();
-    const data = require("../../assets/formationsDetails.json");
-    const formationName = Object.keys(data).find((key)=>location.pathname.endsWith(key)); 
-
+    const data = require("../../assets/formations.json");
+    const pathName = location.pathname.split("/").pop();
+    const formation = data.find((obj)=>obj.link === pathName); 
     return (
         <div className="formation-details">
-            <h2>{ data[formationName].name}</h2>
-            <h3>{ data[formationName].place}</h3>
-            <h3>{ data[formationName].date}</h3>
-            <p>{ data[formationName].introduction}</p>
+            <h2>{ formation.name}</h2>
+            <h3>{formation.place}</h3>
+            <h3>{ formation.date}</h3>
+            <p>{ formation.detail}</p>
             <h3>Compétences</h3>
             <ul>
-                {Object.values(data[formationName].skills).map((skill, index) => (
+                {Object.values(formation.skills).map((skill, index) => (
                 <li key={index}>{skill}</li>
                 ))}
             </ul>
