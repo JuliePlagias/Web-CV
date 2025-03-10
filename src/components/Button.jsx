42f9746dc7e4ac283671d5d9f61b742f11223616
JuliePlagias <julieplagias@gmail.com>
2025-03-10 13:58:10 +0100

    Upd - button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Button({text,to, className=""}){
     return (
-        <div className={`button ${className}`}>
+
+        <button className={`button ${className}`}>
             <Link className="link" to={to}>
                 {text}
             </Link>
-        </div>
+        </button>
     );
 }
