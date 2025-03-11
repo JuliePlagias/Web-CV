2c90791c6788eaf07470a6b481cef621f33f4515
JuliePlagias <julieplagias@gmail.com>
2025-03-11 15:41:43 +0100

    Upd - project json and project detail component

--- src/components/pages/ProjectDetail.jsx
+++ src/components/pages/ProjectDetail.jsx
@@ -1,7 +1,22 @@
+import { useLocation } from "react-router-dom";
+
 export default function ProjectDetail(){
+    const location = useLocation();
+    const data = require("../../assets/projects.json");
+    const pathName = location.pathname.split("/").pop();
+    const project = data.find((obj)=>obj.link === pathName); 
+    
     return (
         <div>
-            
+            <h2>{project.name}</h2>
+            <h3>{project.place}</h3>
+            <h3>{project.time}</h3>
+            <p>{project.detail}</p>
+            <h3>Compétences</h3>
+            {/* list of skills */}
+            {/* Vidéo du projets si une existante , ou lien vers la page steam et donc la vidéo*/}
+            {/* List of projects pictures*/}
+            <a href={project.projectLink} target="_blank" rel="noopener noreferrer">Accéder aux projet</a>
         </div>
     );
 }
