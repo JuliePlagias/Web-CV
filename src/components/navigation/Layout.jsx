9e8745369faf9d309a798113671c41ba421f0c24
JuliePlagias <julieplagias@gmail.com>
2025-03-25 15:02:17 +0100

    Add - basic footer WIP

--- src/components/navigation/Layout.jsx
+++ src/components/navigation/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout(){
     return (
         <div>
           <Navbar /> 
           <Outlet /> {/* Affiche la page demandée (Informations, Projets, etc.) */}
+          <Footer/>
         </div>
       );
 }
