8a04e4d5676446ad7f7075010db3747f3e7711ce
JuliePlagias <julieplagias@gmail.com>
2025-03-26 13:36:12 +0100

    Upd - add link to linkedin and github and some style

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -2,8 +2,14 @@ export default function Footer()
 {
     return (
         <div className="footer">
-            <p>Lien linkedin</p>
-            <p>Lien github</p>
+            <p>Retrouver mon Linkedin ici</p>
+            <a href="https://www.linkedin.com/in/julieplagias/" target="blank">
+                <img src="../assets/icons/linkedin.svg" alt="Logo Linkedin"></img>
+            </a>
+            <p>et mon github ici</p>
+            <a href="https://github.com/JuliePlagias" target="blank">
+                <img src="../assets/icons/github.svg" alt="Logo Github"></img>
+            </a>
         </div>
     )
 }
