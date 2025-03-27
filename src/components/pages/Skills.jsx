24877eca1e86ceb17d0bb106e82177b79f4b7b03
JuliePlagias <julieplagias@gmail.com>
2025-03-27 14:00:43 +0100

    Upd - h styles made global rather than in each file

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -4,11 +4,12 @@ export default function Skills(){
     const data = require("../../assets/skills.json");
 
     return (
-        <div className="blockSection">
+        <div className="blockSection skills">
             <h2>Hard skills</h2>
             {
                 data.hard.map((skill, index)=>(
-                    <SkillBlock key={index} name={skill.name} logo={skill.logo} progress={skill.progress}/>
+                    skill.progress?<SkillBlock key={index} name={skill.name} logo={skill.logo} progress={skill.progress}/>
+                    :<li key={index}>{skill.name}</li>
                 ))
             }
             <h2>Soft skills</h2>
