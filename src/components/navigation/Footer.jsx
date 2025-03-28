da79de03f62daf50ce6633fdf910978c92b2eec1
JuliePlagias <julieplagias@gmail.com>
2025-03-28 09:42:20 +0100

    Upd - footer responsive design

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -2,14 +2,18 @@ export default function Footer()
 {
     return (
         <div className="footer">
-            <p>Retrouver mon Linkedin ici</p>
-            <a href="https://www.linkedin.com/in/julieplagias/" target="blank">
-                <img src="../assets/icons/linkedin.svg" alt="Logo Linkedin"></img>
-            </a>
-            <p>et mon github ici</p>
-            <a href="https://github.com/JuliePlagias" target="blank">
-                <img src="../assets/icons/github.svg" alt="Logo Github"></img>
-            </a>
+            <div>
+                <p>Retrouver mon Linkedin ici</p>
+                <a href="https://www.linkedin.com/in/julieplagias/" target="blank">
+                    <img src="../assets/icons/linkedin.svg" alt="Logo Linkedin"></img>
+                </a>
+            </div>
+            <div>
+                <p>et mon github ici</p>
+                <a href="https://github.com/JuliePlagias" target="blank">
+                    <img src="../assets/icons/github.svg" alt="Logo Github"></img>
+                </a>
+            </div>
         </div>
     )
 }
