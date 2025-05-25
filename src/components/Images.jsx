5736aa5dc1f093d647516badddb63511a21e4465
JuliePlagias <julieplagias@gmail.com>
2025-05-25 13:53:56 +0200

    Upd - experiences visualizers

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -3,7 +3,7 @@ export default function Images({file})
     return (
         <div >
             {file.imgs.map(img=>(
-                <img key={img.id} src={`../assets/images/${img.id}.jpg`}  alt={`image du projet ${img.id}`}></img>
+                <img key={img.id} src={`../assets/images/${img.id}`}  alt={`image du projet ${img.id}`}></img>
             ))} 
         </div>
     )
